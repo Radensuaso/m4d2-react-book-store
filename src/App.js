321f07a452d2eddf2112adddf2453500e694cc7c
Radensuaso <andrels9283@gmail.com>
2021-07-06 15:45:54 +0100

    Homework Requirements complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css"
 import MyFooter from "./components/MyFooter"
 import MyNav from "./components/MyNav"
 import MyJumbotron from "./components/MyJumbotron"
+import LatestReleases from "./components/LatestReleases"
 
 function App() {
   return (
     <>
       <MyNav />
       <MyJumbotron />
+      <LatestReleases />
       <MyFooter />
     </>
   )
