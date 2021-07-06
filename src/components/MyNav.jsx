117d348e3055886f8a2dc91ee782fffcdea4ac8b
Radensuaso <andrels9283@gmail.com>
2021-07-06 13:13:14 +0100

    Navbar added!

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,16 @@
+import { Container, Navbar, Nav } from "react-bootstrap"
+
+const MyNav = () => (
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#">ReactBookStore</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#">Home</Nav.Link>
+        <Nav.Link href="#">About</Nav.Link>
+        <Nav.Link href="#">Browse</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+)
+
+export default MyNav
