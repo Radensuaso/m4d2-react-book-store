cf063129b7d01b1f92bd44f060c117c03c1a7bb4
Radensuaso <andrels9283@gmail.com>
2021-07-06 15:18:41 +0100

    Jumbotron done

--- src/components/MyJumbotron.jsx
+++ src/components/MyJumbotron.jsx
@@ -1,13 +1,18 @@
-import { Col, Container, Jumbotron, Row } from "react-bootstrap"
+import { Container, Jumbotron } from "react-bootstrap"
 
 const MyJumbotron = () => (
   <Jumbotron fluid>
     <Container>
-      <h1>Welcome</h1>
-      <h2>Best Book Store Out there</h2>
-      <p>
-        Come in and see for yourself, you'll find the book you're looking for
-      </p>
+      <div>
+        <div>
+          <h1>Welcome</h1>
+          <h2>Best Book Store Out there</h2>
+          <p>
+            Come in and see for yourself, you'll find the book you're looking
+            for
+          </p>
+        </div>
+      </div>
     </Container>
   </Jumbotron>
 )
