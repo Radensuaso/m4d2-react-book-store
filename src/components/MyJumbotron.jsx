09405e061d33e81b94b8453a52aec749f8117d5b
Radensuaso <andrels9283@gmail.com>
2021-07-06 16:00:29 +0100

    Some adjustments to the jumbotron were done

--- src/components/MyJumbotron.jsx
+++ src/components/MyJumbotron.jsx
@@ -1,18 +1,18 @@
-import { Container, Jumbotron } from "react-bootstrap"
+import { Col, Container, Jumbotron, Row } from "react-bootstrap"
 
 const MyJumbotron = () => (
   <Jumbotron fluid>
     <Container>
-      <div>
-        <div>
+      <Row>
+        <Col xs={10} sm={8} md={6}>
           <h1>Welcome</h1>
           <h2>Best Book Store Out there</h2>
           <p>
             Come in and see for yourself, you'll find the book you're looking
             for
           </p>
-        </div>
-      </div>
+        </Col>
+      </Row>
     </Container>
   </Jumbotron>
 )
