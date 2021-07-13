a8b29c79a57340b56ab364f195f34d2ac0fa4f94
Radensuaso <andrels9283@gmail.com>
2021-07-13 15:46:40 +0100

    Gave some touches to the jumbotron title

--- src/components/MyJumbotron.jsx
+++ src/components/MyJumbotron.jsx
@@ -7,10 +7,6 @@ const MyJumbotron = () => (
         <Col xs={10} sm={8} md={6}>
           <h1>StriveBooks</h1>
           <h2>Best Book Store Out there</h2>
-          <p>
-            Come in and see for yourself, you'll find the book you're looking
-            for
-          </p>
         </Col>
       </Row>
     </Container>
